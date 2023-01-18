84ecf6bf12fbf8e2d485705d8a11431ad96d6836
jorge-garciapina <115723820+jorge-garciapina@users.noreply.github.com>
2023-01-18 17:28:13 -0600

    Feedback
    - You are expected to use a single interval or Timer to control the execution of all the functions. Not one for each function
    - The functions should know nothing about any timer details
    - Avoid using non-English variable/function names

--- Chapter8/exercise2.js
+++ Chapter8/exercise2.js
@@ -1,26 +1,41 @@
-// PART 1: CREATION OF THE TIMER CONTROL:
-function timeControler(time, message) {
-  setInterval(function prueba() {
-    console.log(message);
-  }, time * 1000);
+// PART 1: FUNCTION THAT CONTROLS THE EXECUTION
+// OF THE SIMPLE FUNCTIONS:
+// This variable will be used to control the
+// execution of the functions
+let counter = 0;
+function executeFunctions() {
+  // The function will be calles every second
+  // ... counter will provide the seconds that
+  // has passed since the execution negab
+  counter++;
+
+  // Executes every 30 seconds
+  if (counter % 30 === 0) {
+    functionA();
+  }
+
+  // Executes every 60 seconds
+  if (counter % 60 === 0) {
+    functionB();
+  }
+
+  // Executes every 75 seconds
+  if (counter % 75 === 0) {
+    functionC();
+  }
 }
 
-// PART 2: CREATION OF THE FUNCTIONS:
-// functionA will execute every 30 seconds
-function functionA(message) {
-  timeControler(30, message);
+// PART 2: SIMPLE FUNCTIONS
+function functionA() {
+  console.log("Function A");
 }
 
-// functionB will execute every 60 seconds (every minute)
-function functionB(message) {
-  timeControler(60, message);
+function functionB() {
+  console.log("Function B");
 }
 
-// functionC will execute every 75 seconds (1 minute 15 seconds)
-function functionC(message) {
-  timeControler(75, message);
+function functionC() {
+  console.log("Function C");
 }
 
-functionA("A");
-functionB("B");
-functionC("C");
+setInterval(executeFunctions, 1000);
