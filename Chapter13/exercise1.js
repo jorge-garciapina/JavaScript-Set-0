2fffb7b69e409a1ca17f73cfd41726c08154dea3
jorge-garciapina <115723820+jorge-garciapina@users.noreply.github.com>
2023-01-19 13:21:50 -0600

    Feedback
    - avoid using non-English variable/function names
    - avoid manipulating the rendered DOM on every loop iteration
    - if you want all of your grid cells to have the same style, create a CSS class instead
    - make use of event delegation
    - avoid using innerHTML to manipulate content

--- Chapter13/exercise1.js
+++ Chapter13/exercise1.js
@@ -4,77 +4,51 @@
 
 function gridCreation(side) {
   // This is the parent container. It is the only div defined in the HTML.
-  const elemento = document.getElementById("div1");
+  const element = document.getElementById("div1");
 
+  // Now I used event delegation to manage user inputs:
+  element.addEventListener("click", (event) => {
+    if (event.target.tagName === "DIV") {
+      alert(event.target.textContent);
+    }
+  });
   // -index- is used to number the elements of the grid
   let index = 0;
 
-  // This for creates the grid.
-  // The for do the following:
-  // 1) Create a row
-  // 2) Add the columns to every row
+  // Create a Document Fragment to hold the elements
+  // avoiding render the DOM in every iteration
+  const fragment = document.createDocumentFragment();
+  
   for (let i = 0; i <= side - 1; i++) {
     // CREATION OF THE ROW.
     // The code creates the rows in which it will put
     // numbered grids.
     // Every row is a grid:
     const row = document.createElement("div");
-    row.setAttribute("id", "row" + i);
+    row.id = "row" + i;
+    row.className = "row";
 
-    // Some CSS styles:
-    row.setAttribute(
-      "style",
-      " background-color: #fdf498;" +
-        "display: flex;" +
-        "flex-direction: row;" +
-        "width: 80vw; flex-grow: 1;"
-    );
-
-    // We append the row to the main div:
-    elemento.appendChild(row);
 
     // CREATION OF THE COLUMNS
     for (let j = 0; j <= side - 1; j++) {
       // Element that will go inside the row:
       const rowElement = document.createElement("div");
-
-      // The id will be used to alert the number inside the div:
-      rowElement.setAttribute("id", "element" + String(index));
-
-      // Some styles
-      rowElement.setAttribute(
-        "style",
-        "color:white;" +
-          "font-size: 7rem;" +
-          "background-color: #7bc043;" +
-          "flex-grow: 1;" +
-          "display: flex;" +
-          "justify-content: center;" +
-          "align-items: center;" +
-          "color: white;" +
-          "border: solid;"
-      );
-
-      // Function to alert the id as specified in exercise:
-      function alerta() {
-        const numeric = /\d/g;
-        let pat = rowElement.id.match(numeric).join("");
-        alert(Number(pat));
-      }
-
-      // Adding the functionality to the rowElement
-      rowElement.addEventListener("click", alerta);
+      
+      // A class to define CSS styles:
+      rowElement.className = "row-element";
 
       // Text inside the divs
-      rowElement.innerHTML = String(index);
+      rowElement.textContent = String(index);
 
       // Append the rowElements to the row
-      document.getElementById("row" + String(i)).appendChild(rowElement);
+     row.appendChild(rowElement);
       index++;
     }
+    fragment.appendChild(row);
   }
 
-  console.log(document.body.children);
+  // Append the fragment to the main div
+  element.appendChild(fragment);
 }
 
 // It is possible to change the size of the grid
