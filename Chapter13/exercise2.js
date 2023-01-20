df33ec6056881d6919ebea255077a6ff58e73743
jorge-garciapina <115723820+jorge-garciapina@users.noreply.github.com>
2023-01-20 12:06:32 -0600

    Feedback

--- Chapter13/exercise2.js
+++ Chapter13/exercise2.js
@@ -1,35 +1,35 @@
-// -counter- saves the number of times the button has
-// been activated
-let counter = 1;
-function buttonConfig() {
-  // In this if we limit the button to only activate 3 times
-  if (counter <= 3) {
-    // This is to modify the header:
-    const header = document.getElementById("header1");
-    if (3 - counter === 0) {
-      // Text that appears once the user has reached the limit.
-      header.innerHTML = "You cannot push the button anymore";
-    } else {
-      // Text that changes as user activates the button
-      header.innerHTML =
-        "You can push the button " + String(3 - counter) + " times";
-    }
+function increaseValue() {
+  let counter = 1; // private variable, only accessible within this function
+  return function () {
+    // I decided to use a IIFE to manage the behavior of the DOM elements
+    (() => {
+      // This is to modify the header:
+      const header = document.getElementById("header1");
+      if (3 - counter === 0) {
+        // Text that appears once the user has reached the limit.
+        header.textContent = "You cannot push the button anymore";
+      } else {
+        // Text that changes as user activates the button
+        header.textContent =
+          "You can push the button " + String(3 - counter) + " times";
+      }
 
-    // This is the location of the main container (the meter element)
-    const mainContainer = document.getElementById("meter");
-    // To create the elements that will serve as markers
-    // of the meter elements
-    const childElement = document.createElement("div");
-
-    // Attributes of the children elements
-    childElement.setAttribute(
-      "style",
-      "  height: 100%;" + "width: 33.33333%;" + "background-color: #005b96;"
-    );
-
-    // Append the children elements to the meter counter
-    mainContainer.appendChild(childElement);
-  }
-
-  counter++;
+      // This is the location of the main container (the meter element)
+      const meterIndicator = document.getElementById("meter");
+      meterIndicator.setAttribute("value", String(counter * 50));
+    })();
+    return counter++;
+  };
 }
+
+let increase = increaseValue();
+
+const buttonInDOM = document.getElementById("bttn");
+buttonInDOM.onclick = () => {
+  let limit = increase();
+  if (limit > 2) {
+    buttonInDOM.onclick = () => {
+      alert("You have reached the liimt");
+    };
+  }
+};
