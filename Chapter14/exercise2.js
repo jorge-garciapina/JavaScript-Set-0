213b70d8c8a89f86daafd84772a470a16240f975
jorge-garciapina <115723820+jorge-garciapina@users.noreply.github.com>
2023-01-23 11:34:12 -0600

    Feedback
    - Most of the work can be done through CSS. Avoid using JS to manipulate the text

--- Chapter14/exercise2.js
+++ Chapter14/exercise2.js
@@ -10,64 +10,21 @@ for (elmnt of textP1) {
     textP2.push(elmnt.replace("\n", ""));
   }
 }
-// -------------------- END PART 1-----------------------
 
-// -------------------- PART 2: MAIN FUNCTIONALITY------------------
-// This is the function that transforms the text into 2, 3 or 4 columns
-// In principle it could work fine with more columns.
-function columnAdd(elemnt, columns) {
-  // This part refresh the main container every time a button is pushed
-  // it avoids overlappings.
-  elemnt.innerHTML = "";
-
-  // This part defines the number of columns in the grid,
-  // it is important to say that the grid layout is defined in the CSS.
-  elemnt.setAttribute(
-    "style",
-    "grid-template-columns: repeat(" + String(columns) + ", auto [col-start]);"
-  );
-
-  // The columns are paragraphs created and added to the main container
-  // the text will go inside this paragraphs.
-  // This for creates that p's and put the text inside.
-  for (let i = 1; i <= columns; i++) {
-    // Definition of the p's (columns)
-    const colmn = document.createElement("p");
-    // Style of the columns
-    colmn.setAttribute(
-      "style",
-      "background-color: white; border:solid; margin:0;"
-    );
-    //------------- Start: Adding the text--------------
-    // The text will be the inner HTML of the column
-    colmn.innerHTML = textP2
-      .slice(
-        ((i - 1) * textP2.length) / columns,
-        (i * textP2.length) / columns - 1
-      )
-      .join(" ");
-    //------------- End: Adding the text--------------
-
-    // Finally, we append the column as a child of the main div
-    elemnt.appendChild(colmn);
-  }
-}
-// -------------------- END PART 2-----------------------
-
-// -------------------- PART 3: BUTTONS:-----------------------
+// -------------------- PART 2: BUTTONS:-----------------------
 // All this buttons use the function defined previously
 function twoColumns() {
-  const mainContainer = document.getElementById("text-container");
-  columnAdd(mainContainer, 2);
+  const mainContainer = document.getElementById("txt");
+  mainContainer.style.columnCount = 2;
 }
 
 function threeColumns() {
-  const mainContainer = document.getElementById("text-container");
-  columnAdd(mainContainer, 3);
+  const mainContainer = document.getElementById("txt");
+  mainContainer.style.columnCount = 3;
 }
 
 function fourColumns() {
-  const mainContainer = document.getElementById("text-container");
-  columnAdd(mainContainer, 4);
+  const mainContainer = document.getElementById("txt");
+  mainContainer.style.columnCount = 4;
 }
-// -------------------- END PART 3-----------------------
+// -------------------- END PART 2-----------------------
