a670ca22856b2daeaffe5abca57aafd51c9853f9
jorge-garciapina <115723820+jorge-garciapina@users.noreply.github.com>
2023-03-02 14:44:16 -0600

    Feedback

--- Chapter7/exercise1.js
+++ Chapter7/exercise1.js
@@ -1,40 +1,45 @@
 function hex2Rgb(hex) {
   // PART 1: BASIC DEFINITIONS
-  // Defenition 1: Lower case the input to avoid capitalization problems.
+  // Definition 1: Lower case the input to avoid capitalization problems.
   hex = hex.toLowerCase();
 
-  // Defenition 2: Object that will be used to transform (hexadecimal to decimal)
-  let hexaValues = { a: 10, b: 11, c: 12, d: 13, e: 14, f: 15 };
+  // Definition 2: Pattern that will be used to extract the information in input.
+  let regex = /^#?([0-9a-f]{3}|[0-9a-f]{6})$/i;
+  let match = hex.match(regex);
 
-  // Defenition 3: Patters that will be used to extract the information in input.
-  let regex = /((?<=#)[a-f0-9]{2})|((?<=[a-f0-9]{2})[a-f0-9]{2})|([a-f0-9]{2}$)/g;
+  // If the input doesn't match the pattern, return null
+  if (!match) {
+    return null;
+  }
 
-  let matches = hex.match(regex);
+  // Extract the hexadecimal color from the match
+  let hexColor = match[1];
 
   // PART 2: CONVERSION HEXADECIMAL TO DECIMAL:
   // Variable to save the output colors.
   let colors = [];
 
-  // This for will loop over the patterns
-  for (let color of matches) {
-    // ----------------START: Loop for conversion:--------------
-    let i = 1;
-    let result = 0;
-    for (elmnt of color) {
-      if (Object.keys(hexaValues).indexOf(elmnt) !== -1) {
-        result += Math.pow(16, 2 - i) * hexaValues[elmnt];
-      } else {
-        // console.log(elmnt);
-        result += Math.pow(16, 2 - i) * elmnt;
-      }
-      i++;
-    }
-    colors.push(result);
-  }
-  // ----------------END: Loop for conversion:--------------
+  // Loop over the hex digits in pairs
+  for (let i = 0; i < hexColor.length; i += 2) {
+    // Extract the pair of hex digits
+    let hexPair = hexColor.substring(i, i + 2);
 
-  return "rgb(" + colors.join(",") + ")";
+    // Convert the pair of hex digits to decimal
+    let dec = parseInt(hexPair, 16);
+
+    // Add the decimal value to the colors array
+    colors.push(dec);
+  }
+
+  return `rgb(${colors.join(", ")})`;
 }
 
-let a = hex2Rgb("#3020ff");
-console.log(a);
+console.log(hex2Rgb("#FF0000"));  // (255,0,0)
+console.log(hex2Rgb("#0000FF"));  // (0,0,255)
+console.log(hex2Rgb("#f0f"));     // (240,15)
+console.log(hex2Rgb("#00a"));     // (0,10)
+console.log(hex2Rgb("#3020ft"));  // null
+console.log(hex2Rgb("#12345"));   // null
+console.log(hex2Rgb("#GGG"));     // null
+console.log(hex2Rgb("#1234567")); // null
+console.log(hex2Rgb("#3020ft"));  // null
