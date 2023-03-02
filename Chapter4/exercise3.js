a2232a00e0a020ace6425e14babe17ea2014241a
jorge-garciapina <115723820+jorge-garciapina@users.noreply.github.com>
2023-03-02 12:36:38 -0600

    Feedback

--- Chapter4/exercise3.js
+++ Chapter4/exercise3.js
@@ -2,25 +2,33 @@
 // a.	Make sure that no other data type can be assigned to that variable.
 // b.	It must hold ONLY numbers.
 
-// If the user gives a data different than a number, the value won't
-// be saved in the object
 class OnlyNumbers {
   constructor(value) {
+    // Call the setValue() method to set the initial value of the object
+    this.setValue(value);
+  }
+
+  // Method to set the value of the object
+  setValue(value) {
+    // Check whether the input value is a number
+    if (typeof value !== "number") {
+      throw new Error("Value must be a number");
+    }
+
+    // If the input value is a number, set the _value property
     this._value = value;
   }
 
-  get value() {
+  // Method to get the value of the object
+  getValue() {
     return this._value;
   }
-  set value(val) {
-    if (typeof val !== "number") {
-      throw new Error("Value must be a number");
-    }
-    this._value = val;
-  }
 }
+
 let numberObject = new OnlyNumbers(5);
-console.log(numberObject.value);
-numberObject.value = 0;
-console.log(numberObject.value);
-numberObject.value = "text"; // Throws an error
+console.log(numberObject.getValue()); // Output: 5
+
+numberObject.setValue(0);
+console.log(numberObject.getValue()); // Output: 0
+
+numberObject.setValue("text"); // Throws an error
