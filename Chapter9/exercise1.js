33ea95e4af1d1e1252c7d395267a1b0f42b6a725
jorge-garciapina <115723820+jorge-garciapina@users.noreply.github.com>
2023-03-02 16:29:00 -0600

    Feedback

--- Chapter9/exercise1.js
+++ Chapter9/exercise1.js
@@ -8,39 +8,17 @@ function dataParse(input) {
     throw new Error("Invalid input");
   }
 
-  // Using `{"prop1": 42, "myFn": "function(a, b) { return a+b+this.prop1;}"}`
-  // we have that:
-  // obj = {prop1:42,
-  //         myFn: "function(a, b) { return a+b+this.prop1;}"
-  //       }
-
   // In this "for" the code evaluates if it is dealing with a function
   // is so, it rewrites the entry, transforming it into a method.
   for (let prop in obj) {
-    // In the "if", I used the condition:
-    // typeof obj[prop] === "string"
-    // because I also use: obj[prop].startsWith("function")
-    // and it fails for types of data different to string.
     if (typeof obj[prop] === "string" && obj[prop].startsWith("function")) {
-      // In this point we have:
-      // obj[myFn] = "function(a, b) { return a+b+this.prop1;}"
-
-      obj[prop] = new Function("return " + obj[prop])();
-      // the Function constructor can be viewd as:
-      // Function("return " + "function(a, b) { return a+b+this.prop1;}")();
-      // which is:
-      // function (){return function(a, b) { return a+b+this.prop1;}()}
-      // This "IIFE" activates the first anonymous function,
-      // thus, we have that the function expression results in:
-      // obj[prop] = function(a, b) { return a+b+this.prop1;}
+      obj[prop] = Function('"use strict";return (' + obj[prop] + ")")();
     }
   }
-  // In this point we have:
-  // obj = {prop1:42,
-  //         myFn: function(a, b) { return a+b+this.prop1;}
-  //       }
+
   return obj;
 }
+
 var str1 = `{"prop1": 42, "myFn": "function myFn1(){ return 'hello world' }"}`;
 let obj1 = dataParse(str1);
 console.log("obj1.prop1= ", obj1.prop1);
