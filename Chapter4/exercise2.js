8405c59d2cae8fae0fad48daccc0088fa9a646e2
jorge-garciapina <115723820+jorge-garciapina@users.noreply.github.com>
2023-03-02 12:24:20 -0600

    Feedback

--- Chapter4/exercise2.js
+++ Chapter4/exercise2.js
@@ -1,25 +1,19 @@
 // 2 Create a function that will recursively go through
 // all of the elements of an array of numbers and add them.
-// The steps folloed by this solution are:
-// 1- The array is:
-// arr = [arr[1],arr[2],arr[3],...arr[n]]
-// 2- The algorith works as follows:
-// Step 1: arr[n]   + addRec([arr[1],arr[2],arr[3],...arr[n-1]])
-// Step 2: arr[n-1] + addRec([arr[1],arr[2],arr[3],...arr[n-2]])
-// Step 3: arr[n-2] + addRec([arr[1],arr[2],arr[3],...arr[n-3]])
-// ...
-// Step m: arr[3] + addRec([arr[1],arr[2])
-// Step n: arr[2] + addRec(arr[1])
 
-function addRec(arr) {
-  // The program will finish when arr have only
-  // one element: arr = [arr[0]]
-  if (arr.length === 1) {
-    return arr[0];
+function addRecHelper(arr, index, sum) {
+  // Base case: If we've reached the end of the array, return the sum.
+  if (index === arr.length) {
+    return sum;
   } else {
-    // The program will iterate over all the elements of the array.
-    return arr[arr.length - 1] + addRec(arr.slice(0, arr.length - 1));
+    // Recursive case: Add the current element to the sum, and move to the next index.
+    return addRecHelper(arr, index + 1, sum + arr[index]);
   }
 }
 
-console.log(addRec([1, 1, 1, 1, 8, 24]));
+function addRec(arr) {
+  // Call the helper function with the initial index of 0 and a sum of 0.
+  return addRecHelper(arr, 0, 0);
+}
+
+console.log(addRec([1, 3, 1, 1, 8, 24]));
