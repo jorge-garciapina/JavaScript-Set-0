b520282c05098ba0735ab1da1238e1f28c1cd574
jorge-garciapina <115723820+jorge-garciapina@users.noreply.github.com>
2023-03-02 17:11:55 -0600

    Feedback

--- Chapter13/exercise2.js
+++ Chapter13/exercise2.js
@@ -1,35 +1,32 @@
 function increaseValue() {
   let counter = 1; // private variable, only accessible within this function
   return function () {
-    // I decided to use a IIFE to manage the behavior of the DOM elements
-    (() => {
-      // This is to modify the header:
-      const header = document.getElementById("header1");
-      if (3 - counter === 0) {
-        // Text that appears once the user has reached the limit.
-        header.textContent = "You cannot push the button anymore";
-      } else {
-        // Text that changes as user activates the button
-        header.textContent =
-          "You can push the button " + String(3 - counter) + " times";
-      }
+    // This is to modify the header:
+    const header = document.getElementById("header1");
+    if (3 - counter === 0) {
+      // Text that appears once the user has reached the limit.
+      header.textContent = "You cannot push the button anymore";
+    } else {
+      // Text that changes as user activates the button
+      header.textContent =
+        "You can push the button " + String(3 - counter) + " times";
+    }
 
-      // This is the location of the main container (the meter element)
-      const meterIndicator = document.getElementById("meter");
-      meterIndicator.setAttribute("value", String(counter * 50));
-    })();
-    return counter++;
+    // This is the location of the main container (the meter element)
+    const meterIndicator = document.getElementById("meter");
+    meterIndicator.setAttribute("value", String(counter * 50));
+
+    counter++;
+    if (counter > 3) {
+      const buttonInDOM = document.getElementById("bttn");
+      buttonInDOM.onclick = () => {
+        alert("You have reached the limit");
+      };
+    }
   };
 }
 
 let increase = increaseValue();
 
 const buttonInDOM = document.getElementById("bttn");
-buttonInDOM.onclick = () => {
-  let limit = increase();
-  if (limit > 2) {
-    buttonInDOM.onclick = () => {
-      alert("You have reached the liimt");
-    };
-  }
-};
+buttonInDOM.onclick = increase;
