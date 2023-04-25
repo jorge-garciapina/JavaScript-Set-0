00d18f8c03677eee6e52b7a5a8418e80da274e64
jorge-garciapina <115723820+jorge-garciapina@users.noreply.github.com>
2023-04-25 11:12:48 -0600

    Feedback April

--- Chapter14/exercise1.js
+++ Chapter14/exercise1.js
@@ -1,136 +1,53 @@
-let closureValueColumns = (function () {
-  let counter = 0;
-  return function () {
-    return counter;
-  };
-})();
+let columnsValue = 0;
+let rowsValue = 0;
 
-let closureValueRows = (function () {
-  let counter = 0;
-  return function () {
-    return counter;
-  };
-})();
-
-// -------------------------------------------------
-// ------------------START: PART 1------------------
-// PART 1: DEFINING THE FUNCTIONALITY + AND - BUTTONS:
-// ----------Start: Columns----------
 // Button 1: (+) for columns
 function moreColumns() {
-  let value = closureValueColumns();
-  value++;
-
-  closureValueColumns = function () {
-    return value;
-  };
-  const columnNumber = document.getElementById("columns-value");
-
-  columnNumber.textContent = closureValueColumns();
+  columnsValue++;
+  document.getElementById("columns-value").textContent = columnsValue;
 }
 
 // Button 2: (-) for columns
 function lessColumns() {
-  let value = closureValueColumns();
-  if (value <= 0) {
-    value = 0;
-  } else {
-    value--;
-  }
-
-  closureValueColumns = function () {
-    return value;
-  };
-  const columnNumber = document.getElementById("columns-value");
-  columnNumber.textContent = closureValueColumns();
+  columnsValue = Math.max(0, columnsValue - 1);
+  document.getElementById("columns-value").textContent = columnsValue;
 }
-// -----------End: Columns-----------
 
-// ----------  Start: Rows ----------
 // Button 3: (+) for rows
 function moreRows() {
-  let value = closureValueRows();
-  value++;
-  closureValueRows = function () {
-    return value;
-  };
-  const columnNumber = document.getElementById("rows-value");
-  if (closureValueRows() <= 0) {
-    columnNumber.textContent = "0";
-  } else {
-    columnNumber.textContent = closureValueRows();
-  }
+  rowsValue++;
+  document.getElementById("rows-value").textContent = rowsValue;
 }
 
 // Button 4: (-) for rows
 function lessRows() {
-  let value = closureValueRows();
-  if (value <= 0) {
-    value = 0;
-  } else {
-    value--;
-  }
-
-  closureValueRows = function () {
-    return value;
-  };
-  const columnNumber = document.getElementById("rows-value");
-
-  columnNumber.textContent = closureValueRows();
+  rowsValue = Math.max(0, rowsValue - 1);
+  document.getElementById("rows-value").textContent = rowsValue;
 }
-// -----------  End: Rows  ----------
 
-// ------------------END: PART 1--------------------
-// -------------------------------------------------
-
-// -------------------------------------------------
-// ------------------START: PART 2------------------
-// PART 2: CONFIGURING THE GRID LAYOUT
 function applyChanges() {
-  // ------START: VALUES OF COLUMNS AND ROWS ------
-  // Extract the values chosen by the user
-  let columns = closureValueColumns();
-
-  let rows = closureValueRows();
-  // ------END: VALUES OF COLUMNS AND ROWS ------
-
-  // -------------------START: MAIN CONTAINER ------------------
-  // CONFIGURING THE MAIN CONTAINER:
-  // Main container (where the grid is):
   const mainContainer = document.getElementById("mainContainer");
 
-  (function deleteOldConfiguration() {
-    while (mainContainer.firstChild) {
-      mainContainer.removeChild(mainContainer.firstChild);
-    }
-  })();
-
-  // This style if for configuring the number of rows that the
-  // grid will have:
-  mainContainer.setAttribute(
-    "style",
-    "grid-template-columns: repeat(" + String(columns) + ", auto [row-start]);"
-  );
-  // -------------------END: MAIN CONTAINER ------------------
-
-  // ----------START: ADDING THE GRID ITEMS ------------------
-  // Create a document fragment to avoid manipulating the rendered DOM on every loop iteration
-  const fragment = document.createDocumentFragment();
-  for (let i = 0; i <= rows * columns - 1; i++) {
-    // Creation of the grid items:
-    const gritItem = document.createElement("div");
-    // Some styles:
-    gritItem.setAttribute(
-      "style",
-      "background-color: red; border:solid; border-color: black"
-    );
-    // append the grid item to the document fragment
-    fragment.appendChild(gritItem);
+  // Remove old grid items
+  while (mainContainer.firstChild) {
+    mainContainer.removeChild(mainContainer.firstChild);
   }
+
+  // Set the number of columns for the grid
+  mainContainer.style.gridTemplateColumns = `repeat(${columnsValue}, auto)`;
+
+  // Create a document fragment to hold the grid items
+  const fragment = document.createDocumentFragment();
+
+  // Add the grid items to the fragment
+  for (let i = 0; i < rowsValue * columnsValue; i++) {
+    const gridItem = document.createElement("div");
+    gridItem.style.backgroundColor = "red";
+    gridItem.style.border = "solid";
+    gridItem.style.borderColor = "black";
+    fragment.appendChild(gridItem);
+  }
+
   // Append the fragment to the main container
   mainContainer.appendChild(fragment);
-  // ----------END: ADDING THE GRID ITEMS ------------------
 }
-
-// ------------------END: PART 2--------------------
-// -------------------------------------------------
