86d66c711a0616fa9a7966ac2ab608606b8cdc27
jorge-garciapina <115723820+jorge-garciapina@users.noreply.github.com>
2023-07-03 12:30:40 -0600

    Feedback July

--- Chapter9/exercise1.js
+++ Chapter9/exercise1.js
@@ -1,14 +1,12 @@
 function dataParse(input) {
   let obj;
 
-  // Create an object from the sanitized input string using the Function constructor
   try {
     obj = new Function("return " + input + ";")();
   } catch (e) {
     throw new Error("Invalid input");
   }
 
-  // Iterate through the object properties and convert string representations of functions into actual functions
   for (let prop in obj) {
     if (typeof obj[prop] === "string" && obj[prop].startsWith("function")) {
       obj[prop] = new Function("return " + obj[prop])();
@@ -18,11 +16,6 @@ function dataParse(input) {
   return obj;
 }
 
-var str1 = `{"prop1": 42, "myFn": "function myFn1(){ return 'hello world' }"}`;
-let obj1 = dataParse(str1);
-console.log("obj1.prop1= ", obj1.prop1);
-console.log("obj1.myFn()= ", obj1.myFn());
-var str2 = `{"prop1": 42, "myFn": "function(a, b) { return a+b+this.prop1;}"}`;
-let obj2 = dataParse(str2);
-console.log("obj2.prop1= ", obj2.prop1);
-console.log("obj2.myFn(1, 5)= ", obj2.myFn(1, 5));
+var str0 = "{prop1: 42, myFn: function(a, b) { return a+b+this.prop1;}}";
+let obj0 = dataParse(str0);
+console.log("obj0.myFn()= ", obj0.myFn(1, 2));
